f48da556be47ad21124648398cd32e606db65c73
linbeau0822 <beau.s.lin1@gmail.com>
2025-12-15 12:18:48 -0500

    commented out code

--- main.tf
+++ main.tf
@@ -45,39 +45,40 @@ module "blog_sg" {
 
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
+# Below is an alternate implementation of defining the security group, but we are using modules for our case
+#resource "aws_security_group" "blog" {
+#  name        = "blog"
+#  description = "Allow http and https in. Allow everything out"
+#
+#  vpc_id = data.aws_vpc.default.id
+#}
 
-  vpc_id = data.aws_vpc.default.id
-}
+#resource "aws_security_group_rule" "blog_http_in" {
+#  type        = "ingress"
+#  from_port   = 80
+#  to_port     = 80
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
+#  security_group_id = aws_security_group.blog.id
+#}
 
-  security_group_id = aws_security_group.blog.id
-}
+#resource "aws_security_group_rule" "blog_https_in" {
+#  type        = "ingress"
+#  from_port   = 433
+#  to_port     = 433
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 433
-  to_port     = 433
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
+#  security_group_id = aws_security_group.blog.id
+#}
 
-  security_group_id = aws_security_group.blog.id
-}
+#resource "aws_security_group_rule" "blog_everything_out" {
+#  type        = "egress"
+#  from_port   = 0
+#  to_port     = 0
+#  protocol    = "-1" #allows all protocols
+#  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
 
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1" #allows all protocols
-  cidr_blocks = ["0.0.0.0/0"] #lets us define which networks we want to allow access on)
-
-  security_group_id = aws_security_group.blog.id
-}
+#  security_group_id = aws_security_group.blog.id
+#}
